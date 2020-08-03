912dc9c9e953b64a8127d8a703ededbfe9b16215
Vladislav Antyufeev <v.antyufeev@list.ru>
2020-08-03 13:11:02 +0300

    Remove unused directive

--- Services/PostgresBackupper.cs
+++ Services/PostgresBackupper.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using Sentry.Protocol;
 using WebUI.Services.Interfaces;
 
 namespace WebUI.Services
