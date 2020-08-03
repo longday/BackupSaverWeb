3318ecc55ef4dfe02609421dc8fac0a3af8fb967
Vladislav Antyufeev <v.antyufeev@list.ru>
2020-08-03 11:46:51 +0300

    Change IAsyncRemover interface

--- Services/Interfaces/IAsyncRemover.cs
+++ Services/Interfaces/IAsyncRemover.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Amazon.S3;
 
 namespace WebUI.Services.Interfaces
 {
@@ -6,7 +7,9 @@ namespace WebUI.Services.Interfaces
     /// Interface for removing
     /// </summary>
     public interface IAsyncRemover
-    {
+    { 
+        AmazonS3Client Client { get; set; }
+        string Bucket { get; set; }
         Task RemoveAsync(int quantity);
     }
 }
