d4e5d7fa774fea4363596b34fa92e91af70bea4d
Vladislav Antyufeev <v.antyufeev@list.ru>
2020-08-03 12:11:19 +0300

    Remove unused service

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,6 @@ namespace WebUI
             services.AddScoped<IAsyncSaver, S3StorageSaver>();
             services.AddScoped<IAsyncReporter, TelegramReporter>();
             services.AddScoped<BackupSaver>();
-            services.AddScoped<AmazonS3Client>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
