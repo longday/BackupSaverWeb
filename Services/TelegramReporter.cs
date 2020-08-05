db35361dc8a286b5560d7a5f493dc593ce6e506b
Programmeris <49879947+Programmeris@users.noreply.github.com>
2020-08-05 12:50:05 +0300

    Delete unused costructor

--- Services/TelegramReporter.cs
+++ Services/TelegramReporter.cs
@@ -3,7 +3,6 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Sentry.Extensibility;
-using Sentry.Infrastructure;
 using Sentry.Protocol;
 
 namespace WebUI.Services
@@ -24,11 +23,6 @@ namespace WebUI.Services
             Client = new HttpClient();
         }
 
-        public TelegramReporter()
-        {
-            _logger = new ConsoleDiagnosticLogger(SentryLevel.Info);
-        }
-        
         public TelegramReporter(string connectionString, IDiagnosticLogger logger)
         {
             ConnectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
