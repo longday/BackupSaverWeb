72d75d9f6ab9a1fdf6208bd9c09e4bf21588ec75
Programmeris <49879947+Programmeris@users.noreply.github.com>
2020-08-05 12:55:33 +0300

    Change IAsyncSaver interface

--- Services/Interfaces/IAsyncSaver.cs
+++ Services/Interfaces/IAsyncSaver.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using Amazon.S3;
 
 namespace WebUI.Services.Interfaces
 {
@@ -7,11 +6,7 @@ namespace WebUI.Services.Interfaces
     /// Interface for async saving
     /// </summary>
     public interface IAsyncSaver
-    {
-        public  AmazonS3Client Client { get; set; }
-        
-        public string Bucket { get; set; }
-        
+    {   
         Task SaveAsync(string source);
     }
 }
