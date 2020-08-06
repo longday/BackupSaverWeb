2c68247e98112e0ad764206866a4c2ccab621cf0
Programmeris <v.antyufeev@list.ru>
2020-08-06 11:00:12 +0300

    Make App component default

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MakeBackupButton from './components/MakeBackupButton'
 
-export function App(): JSX.Element{
+export default function App(): JSX.Element{
     return(
     <>
         <h1>Backup Saver</h1>
