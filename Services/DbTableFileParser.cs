23431896044d40c1e2e25a62b8ead6b623ad8e5e
Programmeris <v.antyufeev@list.ru>
2020-08-11 15:06:59 +0300

    Add IFileParser interface and his default implementation

---
+++ Services/DbTableFileParser.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using WebUI.Services.Interfaces;
+
+namespace WebUI.Services
+{
+    public sealed class DbTableFileParser : IFileParser
+    {
+        public string[] GetParsedValues(string path, params string[] ignoredValues)
+        {
+            if(string.IsNullOrWhiteSpace(path))
+                throw new ArgumentNullException(nameof(path));
+
+            Array.ForEach(ignoredValues, iv => Console.WriteLine(iv));
+            Console.WriteLine();
+
+            var lines = File.ReadAllLines(path)
+                            .Skip(2)
+                            .SkipLast(2)
+                            .Select(s => s.Trim(' '))
+                            .Except(ignoredValues)
+                            .ToArray();
+
+            if(lines == null)
+                throw new DbNotFoundException("Databases not found");
+
+            return lines;
+        }
+    }
+}
---
+++ Services/Interfaces/IFileParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WebUI.Services.Interfaces
+{
+    public interface IFileParser
+    {
+        string[] GetParsedValues(string path, params string[] ignoredValues);
+    }
+}
