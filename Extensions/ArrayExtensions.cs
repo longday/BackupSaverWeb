cadcf0b813ebd1ef754f28cce5950848aae8b4b9
Programmeris <v.antyufeev@list.ru>
2020-08-11 15:08:41 +0300

    Add array extensions

---
+++ Extensions/ArrayExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text;
+
+namespace WebUI.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static string ToFormatString<T>(this T[] array, char separator = ',')
+        {
+            StringBuilder strBuilder = new StringBuilder();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                strBuilder.Append(array[i].ToString() + separator);
+            }
+
+            return strBuilder.ToString();
+        }
+    }
+}
