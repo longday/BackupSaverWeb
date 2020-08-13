84a63a2694b19334daeba61a40e9d70579e9a697
Programmeris <v.antyufeev@list.ru>
2020-08-13 22:37:46 +0300

    Create BasicAuthenticationMiddleware

---
+++ Middleware/BasicAuthenticationMiddleware.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Security.Claims;
+
+namespace WebUI.Middleware
+{
+    public class BasicAuthenticationMiddleware 
+    {
+        private readonly RequestDelegate _next;
+
+        public BasicAuthenticationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string authHeader = context.Request.Headers["Authorization"];
+            if(authHeader != null && authHeader.StartsWith("Basic "))
+            {
+                string authHeaderData = authHeader.Substring("Basic".Length).Trim();
+                var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authHeaderData))
+                                                .Split(':', 2);
+                string login = credentials[0];
+                string password = credentials[1];
+
+                if(IsValidUser(login, password))
+                {
+                    var claims = new[] { new Claim("name", login)};
+                    var identity = new ClaimsIdentity(claims, "Basic");
+                    context.User = new ClaimsPrincipal(identity);
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 401;
+                context.Response.Headers.Append("WWW-Authenticate", "Basic realm=\"dotnetthoughts.net\"");
+            }
+
+            await _next.Invoke(context);
+        }
+
+        private bool IsValidUser(string login, string password)
+        {
+            var correctLogin = Environment.GetEnvironmentVariable("LOGIN") as string;
+            var correctPassword = Environment.GetEnvironmentVariable("PASSWORD") as string;
+
+            return login == correctLogin && password == correctPassword;
+        }
+    }
+}
