264ec94330108d9335becb9fd8db156abc349dfb
Programmeris <v.antyufeev@list.ru>
2020-08-14 17:10:52 +0300

    Replace source code in src

--- Services/Interfaces/IFileParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace WebUI.Services.Interfaces
-{
-    public interface IFileParser
-    {
-        string[] GetParsedValues(string path, params string[] ignoredValues);
-    }
-}
